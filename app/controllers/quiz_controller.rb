8fceda887df9f520e83fd85538a92fbbc1673a1a
niordsid <acsantanab@outlook.com>
2018-02-21 19:44:18 -0500

    Configure total question according to the total number question stored in the database

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,59 +1,67 @@
 class QuizController < ApplicationController
   def index
+    @total = Question.all.count
   end
 
   def new
-	 total = params[:number].to_i
-	 all = Question.all.map {|x| x.id}
-	 session[:questions] = all.sort_by{rand}[0..(total-1)]
+    total = Question.all.count
+    all = Question.all.map(&:id)
+    session[:questions] = all.sort_by { rand }[0..(total - 1)]
 
-	 session[:total]   = total
-	 session[:current] = 0
-	 session[:correct] = 0
+    session[:total]   = total
+    session[:current] = 0
+    session[:correct] = 0
 
-	 redirect_to :action => "question"
+    redirect_to action: 'question'
   end
 
   def question
-	 @current = session[:current]
-	 @total   = session[:total]
+    @current = session[:current]
+    @total = session[:total]
 
-	 if @current >= @total
-		redirect_to :action => "end"
-		return
-	 end
+    if @current >= @total
+      redirect_to action: 'end'
+      return
+    end
 
-	 @question = Question.find(session[:questions][@current])
-	 @choices = @question.choices.sort_by{rand}
+    @question = Question.find(session[:questions][@current])
+    @choices = @question.choices.sort_by { rand }
 
-	 session[:question] = @question
-	 session[:choices] = @choices
+    session[:question] = @question
+    session[:choices] = @choices
   end
 
   def answer
-	 @current = session[:current]
-	 @total   = session[:total]
+    @current = session[:current]
+    @total   = session[:total]
 
-	 choiceid = params[:choice]
+    choiceid = params[:choice]
 
-	 @question = session[:question]
-	 @choices  = session[:choices]
+    @question = session[:question]
+    puts @question
+    @choices = session[:choices]
 
-	 @choice = choiceid ? Choice.find(choiceid) : nil
-	 if @choice and @choice.correct
-		@correct = true
-		session[:correct] += 1
-	 else
-		@correct = false
-	 end
+    @choice = choiceid ? Choice.find(choiceid) : nil
+    if @choice && @choice.correct
+      @correct = true
+      session[:correct] += 1
+    else
+      @correct = false
+    end
 
-	 session[:current] += 1
+    session[:current] += 1
   end
 
   def end
-	 @correct = session[:correct]
-	 @total   = session[:total]
+    @correct = session[:correct]
+    @total = session[:total]
 
-	 @score = @correct * 100 / @total
+    @score = @correct * 100 / @total
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:text)
   end
 end
